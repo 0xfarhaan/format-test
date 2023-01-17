061048dee8d17da99e4644847975ec72d58a141a
0xfarhaan <farhaan@maple.finance>
2023-01-17 11:23:09 +0000

    chore: init from https://github.com/farhaan-ali/foundry-template at e1e4fb9

---
+++ tests/unit/Contract.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.7;
+
+import "../../modules/forge-std/src/Test.sol";
+
+contract ContractTest is Test {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
